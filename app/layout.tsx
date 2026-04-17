cd8f1aaa8c7b677f498553bc8afd81704d33a3e7
Perry Dev <perrydev17@gmail.com>
2026-04-17 10:33:19 -0400

    Scaffold app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider"
+
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
@@ -30,7 +32,13 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >{children}</ThemeProvider></body>
     </html>
   );
 }
